5d14e063e391dabc3a8662b00b71554e29863a9e
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-11 17:18:07 -0400

    broken shit

--- get_lyrics.py
+++ get_lyrics.py
@@ -4,8 +4,18 @@ from credentials import client_access_token
 genius = lyricsgenius.Genius(client_access_token)
 genius.verbose = False
 
-def get_song(title, artist):
-    print(genius.search_song(title, artist).lyrics)
-    return genius.search_song(title, artist).lyrics
 
-get_song("prom king", "saba")
+def get_song_dict(title, artist, clean_ad_libs=False):
+    song = genius.search_song(title, artist).to_dict()
+    song_lyrics = song["lyrics"].encode().decode("utf-8").replace(u"\\u2019", "'")
+    chunks = song_lyrics.split("\n\n")
+    chunked = {}
+    for chunk in chunks:
+        part = chunk[chunk.find("[")+1:chunk.find("]")]
+        chunked[part] = chunk[len(part) + 2:]
+
+    return chunked
+
+title = "baby"
+artist = "justin bieber"
+print(get_song_dict(title, artist))
