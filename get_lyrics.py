abbb696e254d44ddd5a39e38fd2f308da4ed9c1a
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-11 18:30:24 -0400

    string suck

--- get_lyrics.py
+++ get_lyrics.py
@@ -1,16 +1,15 @@
 import lyricsgenius
 from credentials import client_access_token
 import re
+import sys
+import json
 
 genius = lyricsgenius.Genius(client_access_token)
 genius.verbose = False
 
 
 def get_song_dict(title, artist, clean_ad_libs=False):
-    song = genius.search_song(title, artist).to_dict()
-    song_lyrics = song["lyrics"].encode("utf-8").decode().replace(u"\u2019", "'")
-    # for i in range(len(song_lyrics)):
-    #     if song_lyrics[i] == "'"and song_lyrics[i-1 "\\"
+    song_lyrics = genius.search_song(title, artist).to_text().replace("'", QUOTE)
     chunks = song_lyrics.split("\n\n")
     chunked = {}
     for chunk in chunks:
@@ -18,7 +17,7 @@ def get_song_dict(title, artist, clean_ad_libs=False):
         chunk_lyrics = chunk[len(part) + 2:].split("\n")
         cleaned_lyrics = []
         for line in chunk_lyrics:
-            cleaned_line = line.replace("\\", "")
+            cleaned_line = ("" + line.encode("utf-8").decode(sys.stdout.encoding))
             cleaned_lyrics.append(str(cleaned_line))
         chunked[part] = cleaned_lyrics[1:]
 
