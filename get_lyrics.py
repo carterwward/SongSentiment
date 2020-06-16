239cf6b5fc5717783645116298f81eb4d6440055
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-06-16 12:43:54 -0400

    fixed quotes and sped up regex

--- get_lyrics.py
+++ get_lyrics.py
@@ -16,8 +16,8 @@ def get_discography(artist):
 def lemmatizer(doc):
     # This takes in a doc of tokens from the NER and lemmatizes them. 
     # Pronouns (like "I" and "you" get lemmatized to '-PRON-', so I'm removing those.
-    doc = [token.lemma_ for token in doc if token.lemma_ != '-PRON-']
-    doc = u' '.join(doc)
+    doc = [token.lemma_ for token in doc if token.lemma_ != "-PRON-"]
+    doc = u"".join(doc)
     return nlp.make_doc(doc)
 
 def remove_stopwords(doc):
@@ -37,14 +37,15 @@ def get_song_dict(title, artist, clean_ad_libs=False):
     song_lyrics = genius.search_song(title, artist).to_text()
     chunks = song_lyrics.split("\n\n")
     chunked = {}
+    exprs = r"\s*\([^)]*\)\s*"
     for chunk in chunks:
-        part = chunk[chunk.find("[")+1:chunk.find("]")]
+        part = chunk[chunk.find('[')+1:chunk.find(']')]
 
-        chunk_lyrics = chunk[len(part) + 2:].split("\n")
+        chunk_lyrics = chunk[len(part) + 2:].split('\n')
         cleaned_lyrics = []
 
         for line in chunk_lyrics:
-            line = re.sub(r'\s*\([^)]*\)\s*', '', line)
+            line = re.sub(exprs, '', line)
 
             if line != "":
                 cleaned_lyrics.append(r"{}".format(str(line)))
