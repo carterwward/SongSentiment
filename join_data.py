1f4ab9c2d7525603a4719c33ca3ad1ceffb2b85d
carterwward <Carter.Ward@uvm.edu>
2020-10-29 16:49:29 -0400

    stuff

--- join_data.py
+++ join_data.py
@@ -2,4 +2,17 @@ from get_valence import get_valence
 from get_lyrics import get_discography
 
 def join_spotify_genius(artist):
+    print('hi')
     
+# TODO: implement getting the full disocgraphy for all features artist in a given artists full disography
+# I realized for this to work we also have to be calling the spotify API, so maybe we should put this function in a different file
+def get_featured_artist_discography(artist):
+# set is underordered and has unique elements
+    artist_set = set()
+    print("...")
+    primary_artist_discography = get_discography(artist)
+    for c in primary_artist_discography:
+        artist_set.add(primary_artist_discography[c]["features"])
+
+
+    print(artist_set)
