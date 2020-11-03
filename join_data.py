082f9ae2b568be2d86e4315539106f18f2de1cc1
carterwward <Carter.Ward@uvm.edu>
2020-11-03 10:34:31 -0500

    stuff

--- join_data.py
+++ join_data.py
@@ -8,11 +8,11 @@ def join_spotify_genius(artist):
     print("in spot not genius Keys:", set(spotify_dict.keys()) - set(genius_dict.keys()), '\n')
     print("in genius not spot Keys:", set(genius_dict.keys()) - set(spotify_dict.keys()), '\n')
     print("Common Keys:", common_keys)
+    print("len of discog", len(common_keys))
     full_dict = {}
     for key in common_keys:
         song_dict = {}
-        print(spotify_dict.keys())
-        print(genius_dict)
+        # print(spotify_dict.keys())
         song_dict['valence'] = spotify_dict[key]['valence']
         song_dict['album'] = spotify_dict[key]['album']
         song_dict['lyrics'] = genius_dict[key]['lyrics']
@@ -22,16 +22,5 @@ def join_spotify_genius(artist):
     return full_dict
 
     
-# TODO: implement getting the full disocgraphy for all features artist in a given artists full disography
-# I realized for this to work we also have to be calling the spotify API, so maybe we should put this function in a different file
-def get_featured_artist_discography(artist):
-# set is underordered and has unique elements
-    artist_set = set()
-    print("...")
-    primary_artist_discography = get_discography(artist)
-    for c in primary_artist_discography:
-        artist_set.add(primary_artist_discography[c]["features"])
 
-
-    print(artist_set)
-join_spotify_genius('jpegmafia')
+# join_spotify_genius('jpegmafia')
---
+++ recursively_scrape.py
@@ -0,0 +1,14 @@
+from join_data import join_spotify_genius
+
+# TODO: implement getting the full disocgraphy for all features artist in a given artists full disography
+# I realized for this to work we also have to be calling the spotify API, so maybe we should put this function in a different file
+def get_featured_artist_discography(artist):
+# set is underordered and has unique elements
+    artist_set = set()
+    print("...")
+    primary_artist_discography = get_discography(artist)
+    for c in primary_artist_discography:
+        artist_set.add(primary_artist_discography[c]["features"])
+
+
+    print(artist_set)
