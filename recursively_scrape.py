54b22c3c7a3fcd08e4a94fb81844056547e49ca5
Cameron Hudson <camphudson@gmail.com>
2020-11-03 13:22:14 -0500

    Update recursively_scrape.py
    working on recurisve, had to change structure

--- recursively_scrape.py
+++ recursively_scrape.py
@@ -3,12 +3,16 @@ from join_data import join_spotify_genius
 # TODO: implement getting the full disocgraphy for all features artist in a given artists full disography
 # I realized for this to work we also have to be calling the spotify API, so maybe we should put this function in a different file
 def get_featured_artist_discography(artist):
-# set is underordered and has unique elements
+    # set is underordered and has unique elements
     artist_set = set()
     print("...")
-    primary_artist_discography = get_discography(artist)
-    for c in primary_artist_discography:
-        artist_set.add(primary_artist_discography[c]["features"])
+    primary_artist_discography = join_spotify_genius(artist)
 
+    for id, items in primary_artist_discography.items():
+        for key in items:
+            print(items[key])
+    
+    # return artist_set
+    
 
-    print(artist_set)
+get_featured_artist_discography("JPEGMAFIA")
