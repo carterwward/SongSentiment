5e06882ef4b19716a0baa007bea6df42633273f7
carterwward <Carter.Ward@uvm.edu>
2020-11-04 14:39:03 -0500

    Update firebase_write.py

--- firebase_write.py
+++ firebase_write.py
@@ -1,8 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-  'projectId': 'music-bot-b24c3',
-})
+cred = credentials.Certificate('firebase_cred.json')
+firebase_admin.initialize_app(cred)
 db = firestore.client()
