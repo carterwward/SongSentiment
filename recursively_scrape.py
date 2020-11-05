cf967ef4c66acd13a9557994f0304357eb1b4d1c
carterwward <Carter.Ward@uvm.edu>
2020-11-04 20:12:49 -0500

    Update recursively_scrape.py

--- recursively_scrape.py
+++ recursively_scrape.py
@@ -1,20 +1,30 @@
 from join_data import join_spotify_genius
 
-# TODO: implement getting the full disocgraphy for all features artist in a given artists full disography
-# I realized for this to work we also have to be calling the spotify API, so maybe we should put this function in a different file
-def get_featured_artist_discography(artist_set, artist):
-    # set is underordered and has unique elements
-    artist_set = set()
-    counter = 0
-    print("...")
+# Function will recursively scrape discogs by features and stop when it has 100 discogs
+# The parameters are a set of the artists we have already scrapped
+    # A dictionary to contain all the artists data (the dict passed in initially will be empty)
+    # and the artist name we are looking at features/discog of
+def get_featured_artist_discography(discog_set, artist_dict, artist):
+
+    # check if the length of the keys of artist dict is already 100 or greater
+        # if yes, just return the artist dict passed in
+
+    # Get primary artist discog
     primary_artist_discography = join_spotify_genius(artist)
+
+    # add primary artist discography to artist_dict
+
+    # iterate over dictionary in discography dict
     for id, items in primary_artist_discography.items():
+
+        # iterate over featured artists in each featured list
         for artist in items['features']:
-            artist_set.add(artist)
-            counter = len(artist_set)
+            # if artist not in discog set and artist not in keys of artist dict
+                # call get_featured_artist_discography and read into sub_artist dict
+                # add only new keys to artist dict
        
                    
-
+    return artist_dict
     
 
-get_featured_artist_discography("redveil")
+# get_featured_artist_discography("redveil")
