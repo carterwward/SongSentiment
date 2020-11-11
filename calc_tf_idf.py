23a9e01561dafe68eff9758caf21e148c0f01597
carterwward <Carter.Ward@uvm.edu>
2020-11-11 16:43:29 -0500

    something is wrong

--- calc_tf_idf.py
+++ calc_tf_idf.py
@@ -1,3 +1,24 @@
-from firebase import read_song_dict
-from process import tokenizer
+from firebase import read_song_dict, read_artist_dict
+from process import lyric_tokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 
+def calculate_tf_idf():
+    print('h')
+
+tfidf_vectorizer = TfidfVectorizer(tokenizer =lyric_tokenizer)
+
+artist_name = "joy division"
+corpus = [song_dict['lyrics'] for song_dict in read_artist_dict(artist_name).values()]
+lyrics = read_song_dict(artist_name, 'isolation')['lyrics']
+tfidf_values = tfidf_vectorizer.fit_transform(corpus)
+response = tfidf_vectorizer.transform([lyrics])
+
+feature_array = np.array(tfidf_vectorizer.get_feature_names())
+tfidf_sorting = np.argsort(response.toarray()).flatten()[::-1]
+
+n = 3
+
+top_n = feature_array[tfidf_sorting]
+
+print(top_n)
--- process.py
+++ process.py
@@ -31,26 +31,27 @@ def remove_stopwords(doc):
     return doc
 
 
-def tokenizer(doc):
+def lyric_tokenizer(doc):
     nlp = spacy.load('en_core_web_sm')
-    stops = ["yeah", '\n', 'intro', 'hook', 'verse', 'yes', 'oh', 'chorus']
+    stops = ["yeah", '\n', 'intro', 'hook', 'verse', 'yes', 'oh', 'chorus', 'like', 'hey', 'okay',
+    'uh', 'blah', 'ooooooh', 'woah', 'la', 'aight', 'whoa', 'til', 'o', 'huh']
     nlp.Defaults.stop_words.update(stops)
     nlp.add_pipe(lemmatizer, name="lemmatizer", after="ner")
     nlp.add_pipe(remove_stopwords, name="stopwords", last=True)
     return nlp(doc)
 
 
-if __name__ == "__main__":
-    #gets an artist's full dictionary
-    discog = read_artist_dict("99 neighbors")
-    for song in discog.values():
-        lyrics = song["lyrics"]
-        lyrics = lyrics.lower()
-        doc = tokenizer(lyrics)
-        print(doc,"\n\n")
-    #gets just an artist's song
-    # song = read_song_dict()
-    # lyrics = song['lyrics']
-    # doc = tokenizer(lyrics)
-    # print(doc)
+# if __name__ == "__main__":
+#     #gets an artist's full dictionary
+#     discog = read_artist_dict("99 neighbors")
+#     for song in discog.values():
+#         lyrics = song["lyrics"]
+#         lyrics = lyrics.lower()
+#         doc = tokenizer(lyrics)
+#         print(doc,"\n\n")
+#     #gets just an artist's song
+#     # song = read_song_dict()
+#     # lyrics = song['lyrics']
+#     # doc = tokenizer(lyrics)
+#     # print(doc)
 
