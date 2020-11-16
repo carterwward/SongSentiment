7c02d93e16830acad7415b48e87a3836b9618d4c
carterwward <Carter.Ward@uvm.edu>
2020-11-15 19:32:32 -0500

    Update calc_tf_idf.py

--- calc_tf_idf.py
+++ calc_tf_idf.py
@@ -4,6 +4,25 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import pandas as pd
 
+def get_song_tf_idf(artist_name, song_name):
+    tfidf_vectorizer=TfidfVectorizer(analyzer='word', lowercase=True)
+    song_name = song_name.lower()
+    # TODO: add same cleaning as song names from genius and spotify
+
+    data_dict = read_artist_dict(artist_name)
+    corpus = [' '.join(lyric_tokenizer(song_dict['lyrics'])) for song_dict in data_dict.values()]
+    tfidf_vectorizer_vectors=tfidf_vectorizer.fit_transform(corpus)
+
+    song_index = list(data_dict.keys()).index(song_name)
+    song_vector = tfidf_vectorizer_vectors[song_index]
+    df = pd.DataFrame(song_vector.T.todense(), index=tfidf_vectorizer.get_feature_names(), columns=["tfidf"]) 
+    df = df.sort_values(by=["tfidf"],ascending=False)
+    print(df.head(20))
+    # print(song_vector.toarray())
+
+
+get_song_tf_idf('saba', "gps")
+
 def build_model():
     # We will actually be getting all of the lyrics and valence scores for every discography, but for now we will use just one
     artist_name = "joy division"
@@ -41,4 +60,4 @@ def build_model():
 # df = df.sort_values(by=["tfidf"],ascending=False)
 # print(df)
 
-build_model()
+# build_model()
